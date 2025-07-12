fe81026fb5347c666c91415b1b9f626f9aac4b16
MaykonRO <maykonro2004@gmail.com>
2025-07-12 16:23:23 -0300

    se sincronizado vai adicionar o consumo da api viacep
    - tenho que adicionar o lock nos inputs
    - tenho que adicionar o callback do lock input
    - fazer a mascara do cep funcionar com o meu site

--- src/scripts/services/viacep.js
+++ src/scripts/services/viacep.js
@@ -1,11 +1,53 @@
-/*const cep = document.getElementById("cepClientInput");
+const cep = document.getElementById("cepClientInput");
 
 const soNumero = (numero) => {
     let valor = /^[0-9]+$/.test(numero);
+    return valor;
+};
 
-    console.log(valor);
+
+const cepValido = (cep) => cep.length == 8 && soNumero(cep);
+
+const limpaForm = () => {
+    document.getElementById("ruaClientInput").value = "";
+    document.getElementById("bairroClientInput").value = "";
+    document.getElementById("cidaClientInput").value = "";
+    document.getElementById("estadoClientInput").value = "";
+};
+
+const preencherForm = (endereco) => {
+    document.getElementById("ruaClientInput").value = endereco.logradouro;
+    document.getElementById("bairroClientInput").value = endereco.bairro;
+    document.getElementById("cidaClientInput").value = endereco.localidade;
+    document.getElementById("estadoClientInput").value = endereco.estado;
+};
+
+const pesquisarCep = async() => {
+    limpaForm();
+
+    const cep = document.getElementById("cepClientInput").value.replace("-","");
+
+    const viacep = `https://viacep.com.br/ws/${cep}/json/`;
+
+    if (cepValido(cep)){
+        const dados = await fetch(viacep);
+        const endereco = await dados.json();
+
+        console.log(endereco);
+
+        if(endereco.hasOwnProperty("erro")){
+            document.getElementById("ruaClientInput").value = "Cep nao encontrado";
+        }
+        else{
+            preencherForm(endereco);
+        }
+
+
+    }
 };
 
 cep.addEventListener("input", () => {
     soNumero(cep.value);
-});*/
+});
+
+document.getElementById("cepClientInput").addEventListener("focusout", pesquisarCep);
