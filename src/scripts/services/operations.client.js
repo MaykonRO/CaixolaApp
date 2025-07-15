c0cddbf36a703c976c84add3b078f74acce8271f
MaykonRO <maykonro2004@gmail.com>
2025-07-14 22:41:36 -0300

    se sincronizado adicionar o teste do form
     vai fazer o teste de captura dos dados do formulario para jogar no console

--- src/scripts/services/operations.client.js
+++ src/scripts/services/operations.client.js
@@ -1,2 +1,28 @@
 import { DbName, versionDB, db, createDB } from "../connection/conection.js";
+
+let
+    form;
+
+const getForm = (event) => {
+    event.preventDefault();
+
+    const
+        nome = document.getElementById("nomeInput").value,
+        celular = document.getElementById("celInput").value,
+        cep = document.getElementById("cepClientInput").value,
+        casa = document.getElementById("numClientInput").value,
+        rua = document.getElementById("ruaClientInput").value,
+        bairro = document.getElementById("bairroClientInput").value,
+        cidade = document.getElementById("cidaClientInput").value,
+        estado = document.getElementById("estadoClientInput").value;
+
+    console.log(nome, " ", celular, " ", cep, " ", casa, " ", rua, " ", bairro, " ", cidade, " ", estado);
+
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
     
+    form = document.getElementById("formClientes");
+    form.onsubmit = getForm;
+});
